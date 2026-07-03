bac1a1df2c0ddf0e2be6ae9a42e6a45e371e51eb
Ryan Houben <connect@ryanhouben.com>
2026-07-03 12:51:21 +0200

    - Add web api layer

---
+++ src/Trion.Infrastructure/Persistence/Migrations/20260703094019_Initial.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Trion.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "activities",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_activities", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "athletes",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_athletes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "coaches",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_coaches", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "activities");
+
+            migrationBuilder.DropTable(
+                name: "athletes");
+
+            migrationBuilder.DropTable(
+                name: "coaches");
+        }
+    }
+}
---
+++ src/Trion.WebApi/Program.cs
@@ -0,0 +1,24 @@
+using Scalar.AspNetCore;
+using Trion.Infrastructure;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddOpenApi();
+
+builder.Services.AddInfrastructure(builder.Configuration);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+    app.MapScalarApiReference();
+}
+
+app.UseHttpsRedirection();
+
+app.MapControllers();
+
+app.Run();
